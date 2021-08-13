cb93c078ea7a712f610ff7f5f6fbc02cad763541
Watson-Sei <seinabehack@gmail.com>
2021-08-13 23:05:06 +0900

    add vg kadai process

--- main.go
+++ main.go
@@ -15,16 +15,60 @@ type Tag struct {
 	CreatedAt time.Time `db:"created_at"`
 }
 
+type Article struct {
+	ID        int64     `db:"id"`
+	Title     string    `db:"title"`
+	Body      string    `db:"body"`
+	CreatedAt time.Time `db:"created_at"`
+}
+
+type ArticleTag struct {
+	ID        int64 `db:"id"`
+	ArticleId int64 `db:"article_id"`
+	TagId     int64 `db:"tag_id"`
+}
+
 func main() {
 	db, err := sqlx.Connect("postgres", "host=pg user=admin password=password dbname=sqlx_db sslmode=disable")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	var id int64
-	err = db.QueryRowx(`INSERT INTO tags (name) VALUES ($1) on conflict (name) do UPDATE SET name=$1 RETURNING id`, "Python").Scan(&id)
+
+	tagSlice := []string{"Curl", "HTTP", "HTTP2"}
+	tagIdSlice := []int64{}
+	for _, v := range tagSlice {
+		var tagId int64
+		err := db.QueryRowx(`INSERT INTO tags (name) VALUES ($1) on conflict (name) do UPDATE SET name=$1 RETURNING id`, v).Scan(&tagId)
+		if err != nil {
+			log.Panic(err)
+		}
+		tagIdSlice = append(tagIdSlice, tagId)
+	}
+
+	fmt.Println(tagIdSlice)
+
+	var createId int64
+	err = db.QueryRowx(`INSERT INTO articles (title, body, user_id) VALUES ($1, $2, $3) RETURNING id`, "Title1", "Body1", 1).Scan(&createId)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	fmt.Println(id)
+	fmt.Println(createId)
+
+	for _, t := range tagIdSlice {
+		var articletagId int64
+		err = db.QueryRowx(`INSERT INTO articles_tags (article_id, tag_id) VALUES ($1, $2) RETURNING id`, createId, t).Scan(&articletagId)
+		if err != nil {
+			log.Panic(err)
+		}
+		fmt.Println(articletagId)
+	}
+
+	var articletag []ArticleTag
+	err = db.Select(&articletag, "SELECT * FROM articles_tags")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(articletag)
 }
---
+++ postgresql/initdb/V1__init.sql
@@ -0,0 +1,25 @@
+-- DROP TABLE IF EXISTS `articles`;
+-- DROP TABLE IF EXISTS `tags`;
+-- DROP TABLE IF EXISTS `articles_tags`;
+
+create table if not exists articles(
+    id int generated always as identity primary key,
+    title text not null,
+    body text not null,
+    user_id int not null,
+    created_at timestamptz not null default current_timestamp
+);
+
+create table if not exists tags(
+    id int generated always as identity primary key,
+    name text not null unique,
+    created_at timestamptz not null default current_timestamp
+);
+
+create table if not exists articles_tags(
+    id int generated always as identity primary key,
+    article_id int not null,
+    tag_id int not null,
+    foreign key (article_id) references articles(id),
+    foreign key (tag_id) references tags(id)
+);
